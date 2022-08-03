dca54985dace033cf41fb67d99ef5baf33c05455
Taufan2992 <taufan.inggil@gmail.com>
2022-08-03 12:08:30 +0700

    update

--- src/components/Mycart.js
+++ src/components/Mycart.js
@@ -1,59 +1,121 @@
 import React from "react";
 import bin from "../assets/image/bin.png";
 import upload from "../assets/image/upload.png"
+import Navbaruser from "../assets/Navbaruser";
 
 function Mycart() {
   return (
     <div>
-      <div class="p-5">
-        <div class="px-5 mb-3">
+
+    <Navbaruser/>
+
+      <div className="p-5 mx-5">
+        <div className="px-5 mb-3 text-red">
           <h3>My Cart</h3>
         </div>
-        <div class="px-5">
+        <div className="px-5">
           <p  className="mb-0 text-red">Review your order</p>
         </div>
 
-        <div class="row">
-          <div class="col-8 px-5">
+        <div className="row">
+          <div className="col-8 px-5">
             <hr />
 
-            <div class="card mb-3" style={{ border: "none" }}>
-              <div class="row g-0">
-                <div class="col-md-2">
+            <div className="card mb-3 scroll" style={{ border: "none" }}>
+              <div className="row g-0 mb-2">
+                <div className="col-md-2">
                   <img
                     src="https://mdbcdn.b-cdn.net/wp-content/uploads/2020/06/vertical.webp"
                     alt="Trendy Pants and Shoes"
-                    class="rounded-start"
+                    className="rounded"
                     height={"100px"}
                     width={"100px"}
                     style={{ objectFit: "cover" }}
                   />
                 </div>
-                <div class="col-md-10 row">
-                  <div class="col-md-6">
-                    <div class="card-body">
+                <div className="col-md-10 row">
+                  <div className="col-md-9">
+                    <div className="card-body px-0">
                       <p
-                        class="card-title"
+                        className="card-title text-red"
                         style={{ fontSize: "18px", fontWeight: "900" }}
                       >
                         Title
                       </p>
 
                       <p
-                        class="card-text"
-                        style={{ fontSize: "16px", fontWeight: "800" }}
+                        className="card-text"
+                        style={{ fontSize: "16px", fontWeight: "800", color:"#974A4A" }}
                       >
-                        Topping:
+                        Topping :
+                        <span 
+                        className="text-red ms-1"
+                        style={{fontSize:"14px", fontWeight: "100"}}>
+                          Bill Berry Boba
+                        </span>
                       </p>
                     </div>
                   </div>
 
-                  <div class="col-md-6">
-                    <div class="card-body">
+                  <div className="col-md-3">
+                    <div className="card-body px-0">
                       <p
-                        class="card-title"
+                        className="card-title text-red"
                         style={{
-                          fontSize: "18px",
+                          fontSize: "16px",
+                          fontWeight: "400",
+                          textAlign: "right",
+                        }}
+                      >
+                        Rp. 15.000
+                      </p>
+
+                      <img src={bin} alt="" style={{ float: "right" }} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row g-0">
+                <div className="col-md-2">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/wp-content/uploads/2020/06/vertical.webp"
+                    alt="Trendy Pants and Shoes"
+                    className="rounded"
+                    height={"100px"}
+                    width={"100px"}
+                    style={{ objectFit: "cover" }}
+                  />
+                </div>
+                <div className="col-md-10 row">
+                  <div className="col-md-9">
+                    <div className="card-body px-0">
+                      <p
+                        className="card-title text-red"
+                        style={{ fontSize: "18px", fontWeight: "900" }}
+                      >
+                        Title
+                      </p>
+
+                      <p
+                        className="card-text"
+                        style={{ fontSize: "16px", fontWeight: "800", color:"#974A4A" }}
+                      >
+                        Topping :
+                        <span 
+                        className="text-red ms-1"
+                        style={{fontSize:"14px", fontWeight: "100"}}>
+                          Bill Berry Boba
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="col-md-3">
+                    <div className="card-body px-0">
+                      <p
+                        className="card-title text-red"
+                        style={{
+                          fontSize: "16px",
                           fontWeight: "400",
                           textAlign: "right",
                         }}
@@ -70,30 +132,30 @@ function Mycart() {
 
             <hr />
 
-            <div class="row">
-              <div class=" col-8">
+            <div className="row">
+              <div className="col-8 text-red">
                 <hr />
 
-                <div class="d-flex justify-content-between">
-                  <p class="d-flex">Subtotal</p>
-                  <p class="d-flex">1000</p>
+                <div className="d-flex justify-content-between">
+                  <p className="d-flex">Subtotal</p>
+                  <p className="d-flex">1000</p>
                 </div>
 
-                <div class="d-flex justify-content-between">
-                  <p class="d-flex">Qty</p>
-                  <p class="d-flex">1</p>
+                <div className="d-flex justify-content-between">
+                  <p className="d-flex">Qty</p>
+                  <p className="d-flex">1</p>
                 </div>
 
                 <hr />
 
-                <div class="d-flex justify-content-between">
-                  <p class="d-flex text-success">Total</p>
-                  <p class="d-flex text-success">1000</p>
+                <div className="d-flex justify-content-between">
+                  <p className="d-flex">Total</p>
+                  <p className="d-flex">1000</p>
                 </div>
               </div>
 
-              <div class=" col-4">
-                <label for="upload" style={{ float: "right" }} class="mt-4">
+              <div className=" col-4">
+                <label for="upload" style={{ float: "right" }} className="mt-4">
                   <input type="file" id="upload" hidden />
                   <img src={upload} style={{ float: "right" }}/>
                 </label>
@@ -101,42 +163,44 @@ function Mycart() {
             </div>
           </div>
 
-          <div class=" col-4 px-5">
-            <div class="mb-3">
+          <div className=" col-4 px-5">
+            <div className="mb-3">
               <input
                 type="email"
-                class="form-control mb-2 mt-3"
-                id="name"
+                className="form-control mb-3 mt-3 border-red border-3 inputbg-red"
+                id="placeholdertext"
                 placeholder="Name"
               />
               <input
                 type="email"
-                class="form-control mb-2"
-                id="email"
+                className="form-control mb-3 border-red border-3 inputbg-red"
+                id="placeholdertext"
                 placeholder="Email"
               />
               <input
                 type="email"
-                class="form-control mb-2"
-                id="phone"
+                className="form-control mb-3 border-red border-3 inputbg-red"
+                id="placeholdertext"
                 placeholder="Phone"
               />
               <input
                 type="email"
-                class="form-control mb-2"
-                id="pos"
+                className="form-control mb-3 border-red border-3 inputbg-red"
+                id="placeholdertext"
                 placeholder="Pos Code"
               />
 
               <textarea
-                class="form-control mb-2"
-                id="address"
-                rows="3"
+                className="form-control mb-5 border-red border-3 inputbg-red"
+                id="placeholdertext"
+                rows="4"
                 placeholder="Address"
               ></textarea>
 
-              <button class="container btn btn-primary" type="button">
-                Button
+              <button 
+              className="container btn btn-primary bg-red border-0" 
+              type="button">
+                Pay
               </button>
             </div>
           </div>
